3720de5b531f086d49fb0f9700f034e8356b0bf4
Anaïs <anais@Anaiss-MacBook-Pro.local>
2016-12-21 18:22:11 +0800

    Lib iNIT

---
+++ tests/oauth2_test.py
@@ -0,0 +1,254 @@
+# Copyright 2013 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Unit tests to cover the oauth2 module."""
+
+
+import datetime
+import unittest
+
+
+import httplib2
+import mock
+
+import googleads.common
+import googleads.errors
+import googleads.oauth2
+
+import fake_filesystem
+import fake_tempfile
+from oauth2client.client import AccessTokenRefreshError
+from oauth2client.client import OAuth2Credentials
+from oauth2client.client import SignedJwtAssertionCredentials
+
+
+class GetAPIScopeTest(unittest.TestCase):
+  """Tests for the googleads.oauth2.GetAPIScope function."""
+
+  def setUp(self):
+    self.api_name_adwords = 'adwords'
+    self.api_name_dfp = 'dfp'
+    self.scope_adwords = 'https://www.googleapis.com/auth/adwords'
+    self.scope_dfp = 'https://www.googleapis.com/auth/dfp'
+
+  def testGetAPIScope_adwords(self):
+    self.assertEquals(googleads.oauth2.GetAPIScope(self.api_name_adwords),
+                      self.scope_adwords)
+
+  def testGetAPIScope_badKey(self):
+    self.assertRaises(googleads.errors.GoogleAdsValueError,
+                      googleads.oauth2.GetAPIScope, 'fake_api_name')
+
+  def testGetAPIScope_dfp(self):
+    self.assertEquals(googleads.oauth2.GetAPIScope(self.api_name_dfp),
+                      self.scope_dfp)
+
+
+class GoogleOAuth2ClientTest(unittest.TestCase):
+  """Tests for the googleads.oauth2.GoogleOAuth2Client class."""
+
+  def testCreateHttpHeader(self):
+    """For coverage."""
+    self.assertRaises(
+        NotImplementedError,
+        googleads.oauth2.GoogleOAuth2Client().CreateHttpHeader)
+
+  def testRefresh(self):
+    """For coverage."""
+    self.assertRaises(
+        NotImplementedError,
+        googleads.oauth2.GoogleOAuth2Client().Refresh)
+
+
+class GoogleRefreshTokenClientTest(unittest.TestCase):
+  """Tests for the googleads.oauth2.GoogleRefreshTokenClient class."""
+
+  def setUp(self):
+    self.client_id = 'client_id'
+    self.client_secret = 'itsasecret'
+    self.refresh_token = 'refreshing'
+    https_proxy_host = 'myproxy.com'
+    https_proxy_port = 443
+    self.https_proxy = googleads.common.ProxyConfig.Proxy(https_proxy_host,
+                                                          https_proxy_port)
+    self.proxy_config = googleads.common.ProxyConfig(
+        https_proxy=self.https_proxy)
+    self.access_token_unrefreshed = 'a'
+    self.access_token_refreshed = 'b'
+
+    # Mock out httplib2.Http for testing.
+    self.http = mock.Mock(spec=httplib2.Http)
+    self.opener = self.http.return_value = mock.Mock()
+    self.opener.proxy_info = self.proxy_config.proxy_info
+    self.opener.ca_certs = self.proxy_config.cafile
+    self.opener.disable_ssl_certificate_valiation = (
+        self.proxy_config.disable_certificate_validation)
+
+    # Mock out oauth2client.client.OAuth2Credentials for testing
+    self.oauth2_credentials = mock.Mock(spec=OAuth2Credentials)
+    self.mock_oauth2_credentials = self.oauth2_credentials.return_value = (
+        mock.Mock())
+    self.mock_oauth2_credentials.access_token = self.access_token_unrefreshed
+    self.mock_oauth2_credentials.token_expiry = datetime.datetime(1980, 1, 1,
+                                                                  12)
+
+    def apply(headers):
+      headers['Authorization'] = ('Bearer %s'
+                                  % self.mock_oauth2_credentials.access_token)
+
+    def refresh(mock_http):
+      self.mock_oauth2_credentials.access_token = self.access_token_refreshed
+      self.mock_oauth2_credentials.token_expiry = datetime.datetime.utcnow()
+
+    self.mock_oauth2_credentials.apply = mock.Mock(side_effect=apply)
+    self.mock_oauth2_credentials.refresh = mock.Mock(side_effect=refresh)
+    with mock.patch('oauth2client.client.OAuth2Credentials',
+                    self.oauth2_credentials):
+      self.googleads_client = googleads.oauth2.GoogleRefreshTokenClient(
+          self.client_id, self.client_secret, self.refresh_token,
+          self.proxy_config)
+
+  def testCreateHttpHeader_noRefresh(self):
+    header = {'Authorization': 'Bearer %s' % self.access_token_unrefreshed}
+    self.mock_oauth2_credentials.token_expiry = None
+    self.assertEqual(header, self.googleads_client.CreateHttpHeader())
+
+  def testCreateHttpHeader_refresh(self):
+    header = {u'Authorization': 'Bearer %s' % self.access_token_refreshed}
+
+    with mock.patch('httplib2.Http', self.http):
+      self.assertEqual(header, self.googleads_client.CreateHttpHeader())
+      self.http.assert_called_once_with(
+          ca_certs=None, proxy_info=self.proxy_config.proxy_info,
+          disable_ssl_certificate_validation=(
+              self.proxy_config.disable_certificate_validation))
+      self.mock_oauth2_credentials.refresh.assert_called_once_with(
+          self.opener)
+
+  def testCreateHttpHeader_refreshFails(self):
+    self.mock_oauth2_credentials.refresh.side_effect = AccessTokenRefreshError(
+        'Invalid response 400')
+
+    with mock.patch('httplib2.Http', self.http):
+      self.assertRaises(AccessTokenRefreshError,
+                        self.googleads_client.CreateHttpHeader)
+      self.assertFalse(self.mock_oauth2_credentials.apply.called)
+
+
+class GoogleServiceAccountTest(unittest.TestCase):
+  """Tests for the googleads.oauth2.GoogleServiceAccountClient class."""
+
+  def setUp(self):
+    self.scope = 'scope'
+    self.service_account_email = 'email@email.com'
+    self.private_key_password = 'notasecret'
+    https_proxy_host = 'myproxy.com'
+    https_proxy_port = 443
+    https_proxy = googleads.common.ProxyConfig.Proxy(https_proxy_host,
+                                                     https_proxy_port)
+    self.proxy_config = googleads.common.ProxyConfig(https_proxy=https_proxy)
+    self.https_proxy = '%s:%s' % (https_proxy_host, https_proxy_port)
+    self.access_token_unrefreshed = 'a'
+    self.access_token_refreshed = 'b'
+
+    # Mock out filesystem and file for testing.
+    filesystem = fake_filesystem.FakeFilesystem()
+    tempfile = fake_tempfile.FakeTempfileModule(filesystem)
+    fake_open = fake_filesystem.FakeFileOpen(filesystem)
+    key_file_path = tempfile.NamedTemporaryFile(delete=False).name
+
+    with fake_open(key_file_path, 'w') as file_handle:
+      file_handle.write('IT\'S A SECRET TO EVERYBODY.')
+
+    # Mock out httplib2.Http for testing.
+    self.http = mock.Mock(spec=httplib2.Http)
+    self.opener = self.http.return_value = mock.Mock()
+    self.opener.proxy_info = self.proxy_config.proxy_info
+    self.opener.ca_certs = self.proxy_config.cafile
+    self.opener.disable_ssl_certificate_valiation = (
+        self.proxy_config.disable_certificate_validation)
+
+    # Mock out oauth2client.client.OAuth2Credentials for testing
+    self.oauth2_credentials = mock.Mock(spec=SignedJwtAssertionCredentials)
+    self.mock_oauth2_credentials = self.oauth2_credentials.return_value = (
+        mock.Mock())
+    self.mock_oauth2_credentials.access_token = 'x'
+    self.mock_oauth2_credentials.token_expiry = datetime.datetime(1980, 1, 1,
+                                                                  12)
+
+    def apply(headers):
+      headers['Authorization'] = ('Bearer %s'
+                                  % self.mock_oauth2_credentials.access_token)
+
+    def refresh(mock_http):
+      self.mock_oauth2_credentials.access_token = (
+          self.access_token_unrefreshed if
+          self.mock_oauth2_credentials.access_token is 'x'
+          else self.access_token_refreshed)
+      self.mock_oauth2_credentials.token_expiry = datetime.datetime.utcnow()
+
+    self.mock_oauth2_credentials.apply = mock.Mock(side_effect=apply)
+    self.mock_oauth2_credentials.refresh = mock.Mock(side_effect=refresh)
+    with mock.patch('__builtin__.open', fake_open):
+      with mock.patch('oauth2client.client'
+                      '.SignedJwtAssertionCredentials',
+                      self.oauth2_credentials):
+        self.googleads_client = googleads.oauth2.GoogleServiceAccountClient(
+            self.scope, self.service_account_email, key_file_path,
+            self.private_key_password, proxy_config=self.proxy_config)
+      # Undo the call count for the auto-refresh
+      self.mock_oauth2_credentials.refresh.reset_mock()
+
+  def testCreateHttpHeader_noRefresh(self):
+    header = {'Authorization': 'Bearer %s' % self.access_token_unrefreshed}
+    self.mock_oauth2_credentials.token_expiry = None
+    self.assertEqual(header, self.googleads_client.CreateHttpHeader())
+
+  def testCreateHttpHeader_refresh(self):
+    header = {u'Authorization': 'Bearer %s' % self.access_token_refreshed}
+
+    with mock.patch('httplib2.Http', self.http):
+      self.assertEqual(header, self.googleads_client.CreateHttpHeader())
+      self.http.assert_called_once_with(
+          ca_certs=None, proxy_info=self.proxy_config.proxy_info,
+          disable_ssl_certificate_validation=(
+              self.proxy_config.disable_certificate_validation))
+      self.mock_oauth2_credentials.refresh.assert_called_once_with(
+          self.opener)
+
+  def testCreateHttpHeader_refreshFails(self):
+    self.mock_oauth2_credentials.refresh.side_effect = AccessTokenRefreshError(
+        'Invalid response 400')
+
+    with mock.patch('httplib2.Http', self.http):
+      self.assertRaises(AccessTokenRefreshError,
+                        self.googleads_client.CreateHttpHeader)
+      self.assertFalse(self.mock_oauth2_credentials.apply.called)
+
+  def testIssue123(self):
+    """Test that verifies the fix for Issue #123 on the GitHub Issue Tracker.
+
+    See: https://github.com/googleads/googleads-python-lib/issues/123
+    """
+    with mock.patch('__builtin__.open'):
+      with mock.patch('oauth2client.client'
+                      '.SignedJwtAssertionCredentials',
+                      self.oauth2_credentials):
+        googleads.oauth2.GoogleServiceAccountClient(
+            self.scope, self.service_account_email, '/dev/null',
+            self.private_key_password)
+
+
+if __name__ == '__main__':
+  unittest.main()
---
+++ tests/util_test.py
@@ -0,0 +1,241 @@
+# Copyright 2016 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Unit tests to cover the errors module."""
+
+import logging
+import os
+import re
+import unittest
+from xml.etree import ElementTree
+
+
+
+import googleads.adwords
+import googleads.dfp
+import googleads.util
+import mock
+import suds.transport
+
+
+class PatchesTest(unittest.TestCase):
+  """Tests for the PatchHelper utility."""
+
+  def setUp(self):
+    oauth_header = {'Authorization': 'header'}
+    oauth2_client = mock.Mock()
+    oauth2_client.CreateHttpHeader.return_value = dict(oauth_header)
+
+    # AdWordsClient setup
+    client_customer_id = 'client customer id'
+    dev_token = '?wH47154M4n'
+    user_agent = '4M153rAb13p1l30F53CR37s'
+    self.adwords_client = googleads.adwords.AdWordsClient(
+        dev_token, oauth2_client, user_agent,
+        client_customer_id=client_customer_id)
+
+    # DfpClient setup
+    network_code = '12345'
+    application_name = 'application name'
+    self.dfp_client = googleads.dfp.DfpClient(
+        oauth2_client, application_name, network_code)
+
+  def testPatchedSudsJurkoAppender(self):
+    """Tests to confirm that the appender no longer removes empty objects."""
+    # Create a fake query & statement
+    order_id = 1
+    values = [{
+        'key': 'orderId',
+        'value': {
+            'xsi_type': 'NumberValue',
+            'value': order_id
+        }
+    }, {
+        'key': 'status',
+        'value': {
+            'xsi_type': 'TextValue',
+            'value': 'NEEDS_CREATIVES'
+        }
+    }]
+    query = 'WHERE orderId = :orderId AND status = :status'
+    statement = googleads.dfp.FilterStatement(query, values)
+
+    line_item_service = self.dfp_client.GetService('LineItemService')
+    line_item_service.suds_client.set_options(nosend=True)
+    line_item_action = {'xsi_type': 'ActivateLineItems'}
+    # Perform the ActivateLineItems action.
+    request = line_item_service.performLineItemAction(
+        line_item_action, statement.ToStatement()).envelope
+    line_item_service.suds_client.set_options(nosend=False)
+    # Strip namespace prefixes and parse.
+    parsed_request = ElementTree.fromstring(re.sub('ns[0-1]:', '', request))
+    line_item_action = parsed_request.find('Body').find('performLineItemAction')
+    # Assert that the request includes the action.
+    self.assertTrue(line_item_action is not None)
+
+  def testPatchedSudsJurkoAppenderIssue63(self):
+    """Verifies that issue 63 has been resolved with the patch."""
+    # Create a fake operation to set a Keyword's finalMobileUrls and finalUrls
+    # to a blank value. In the bug, these fields would be excluded from the
+    # output.
+    operations = [{
+        'operator': 'SET',
+        'operand': {
+            'xsi_type': 'BiddableAdGroupCriterion',
+            'adGroupId': 1,
+            'criterion': {
+                'id': 123456,
+                'xsi_type': 'Keyword',
+                'type': 'KEYWORD',
+                'matchType': 'BROAD',
+                'text': 'I AM ERROR',
+            },
+            # These two fields should appear in the request.
+            'finalMobileUrls': {'urls': []},
+            'finalUrls': {'urls': []}
+        }
+    }]
+    adgroup_criterion_service = self.adwords_client.GetService(
+        'AdGroupCriterionService')
+    adgroup_criterion_service.suds_client.set_options(nosend=True)
+    request = adgroup_criterion_service.mutate(operations).envelope
+    adgroup_criterion_service.suds_client.set_options(nosend=False)
+    # Strip namespace prefixes and parse.
+    parsed_request = ElementTree.fromstring(re.sub('ns[0-1]:', '', request))
+    operand = parsed_request.find('Body').find('mutate').find(
+        'operations').find('operand')
+    final_mobile_urls = operand.find('finalMobileUrls')
+    final_urls = operand.find('finalUrls')
+    # Assert that the request includes the empty urls.
+    self.assertTrue(final_mobile_urls is not None)
+    self.assertTrue(final_urls is not None)
+
+  def testPatchedSudsJurkoAppenderIssue78(self):
+    """Verifies that issue 78 has been resolved with the patch."""
+    # Create a fake customer that contains only an empty value for the
+    # trackingUrl template. In the bug, this would be excluded from the output.
+    customer = {'trackingUrlTemplate': '', 'xsi_type': 'Customer'}
+    customer_service = self.adwords_client.GetService('CustomerService')
+    customer_service.suds_client.set_options(nosend=True)
+    request = customer_service.mutate(customer).envelope
+    customer_service.suds_client.set_options(nosend=False)
+    # Strip namespace prefixes and parse.
+    parsed_request = ElementTree.fromstring(re.sub('ns[0-1]:', '', request))
+    tracking_url_template = parsed_request.find('Body').find('mutate').find(
+        'customer').find('trackingUrlTemplate')
+    # Assert that the request includes the empty trackingUrlTemplate.
+    self.assertTrue(tracking_url_template is not None)
+
+
+class GoogleAdsCommonFilterTest(unittest.TestCase):
+  """Tests for the GoogleAdsCommonFilter utility."""
+
+  def setUp(self):
+    self.filter = googleads.util.GetGoogleAdsCommonFilter()
+    self.omitted_text = 'OMITTED'
+    self.dev_token_template = ('<tns:developerToken>%s</tns:developerToken>')
+
+  def testGetGoogleAdsCommonFilter(self):
+    self.assertIs(self.filter, googleads.util.GetGoogleAdsCommonFilter())
+
+  def testFilterAtInfoLevel(self):
+    record = mock.Mock()
+    record.levelno = logging.INFO
+    record.args = [self.dev_token_template % 'test']
+    self.filter.filter(record)
+    self.assertEqual(
+        record.args, (self.dev_token_template % self.omitted_text,))
+
+  def testFilterAtUnfilteredLevel(self):
+    expected_args = (1, 2, 3, 4, 5)
+    record = mock.Mock()
+    record.levelno = logging.DEBUG
+    record.args = expected_args
+    self.filter.filter(record)
+    self.assertEqual(record.args, expected_args)
+
+
+class SudsClientFilterTest(unittest.TestCase):
+  """Tests for the SudsClientFilter utility."""
+
+  def setUp(self):
+    self.filter = googleads.util.GetSudsClientFilter()
+    self.omitted_text = 'OMITTED'
+    self.dev_token_template = 'DevToken'
+    self.location = 'https://www.google.com'
+    test_dir = os.path.dirname(__file__)
+    with open(os.path.join(
+        test_dir, 'test_data/request_envelope_template.txt'), 'r') as handler:
+      self.test_request_envelope_template = handler.read()
+
+  def testGetSudsClientFilter(self):
+    self.assertIs(self.filter, googleads.util.GetSudsClientFilter())
+
+  def testFilterWithSudsClientSOAPMessage(self):
+    record = mock.Mock()
+    soapenv = mock.Mock()
+    soapenv.str.return_value = (self.test_request_envelope_template
+                                % self.dev_token_template)
+    record.msg = 'sending to (%s)\nmessage:\n%s'
+    record.args = [self.location, soapenv]
+    self.filter.filter(record)
+    self.assertEqual(record.args, (self.location,
+                                   (self.test_request_envelope_template
+                                    % self.omitted_text)))
+
+  def testFilterWithSudsClientHeadersMessage(self):
+    record = mock.Mock()
+    record.msg = 'headers = %s'
+    record.args = {'Content-Type': 'text/xml; charset=utf-8',
+                   'Authorization': 'Bearer abc123doremi'}
+    expected_args = {'Content-Type': 'text/xml; charset=utf-8',
+                     'Authorization': self.omitted_text}
+    self.filter.filter(record)
+    self.assertEqual(record.args, expected_args)
+
+
+class SudsTransportFilterTest(unittest.TestCase):
+  """Tests for the SudsTransportFilter utility."""
+
+  def setUp(self):
+    self.filter = googleads.util.GetSudsTransportFilter()
+    self.omitted_text = 'OMITTED'
+    self.dev_token_template = 'DevToken'
+    self.location = 'https://www.google.com'
+    test_dir = os.path.dirname(__file__)
+    with open(os.path.join(
+        test_dir, 'test_data/request_envelope_template.txt'), 'r') as handler:
+      self.test_request_envelope_template = handler.read()
+
+  def testGetSudsTransportFilter(self):
+    self.assertIs(self.filter, googleads.util.GetSudsTransportFilter())
+
+  def testFilterSudsTransportRequest(self):
+    request_instance = suds.transport.Request('https://www.google.com')
+    request_instance.headers = {'User-Agent': 'user-agent',
+                                'Authorization': 'abc123doremi'}
+    request_instance.message = (self.test_request_envelope_template %
+                                self.dev_token_template)
+    expected_headers = {'User-Agent': 'user-agent',
+                        'Authorization': self.omitted_text}
+    record = mock.Mock()
+    record.args = (request_instance,)
+    self.filter.filter(record)
+    self.assertEqual(record.args[0].headers, expected_headers)
+    self.assertEqual(record.args[0].message,
+                     self.test_request_envelope_template % self.omitted_text)
+
+
+if __name__ == '__main__':
+  unittest.main()
